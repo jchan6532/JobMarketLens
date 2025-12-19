d7888ad33a7d32c994ef7ca0d5e5f0308d430029
jchan6532 <justinchan057@gmail.com>
2025-12-19 05:16:41 -0500

    add comment to ignore unused import warning

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -2,6 +2,6 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
-from app.db.models import job_posting
-from app.db.models import skill
-from app.db.models import job_skill
+from app.db.models import job_posting  # noqa: E402, F401
+from app.db.models import skill        # noqa: E402, F401
+from app.db.models import job_skill    # noqa: E402, F401
